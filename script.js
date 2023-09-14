63b4a1896cf8e0df08f03f8515885e7e6d50cf23
dyrk <vlomov182@gmail.com>
2023-09-14 09:36:06 +0300

    full revamp

---
+++ script.js
@@ -0,0 +1,15 @@
+// Select the container div and tabs
+var buttonContainer = document.querySelector('.win-btns');
+var tabs = document.querySelectorAll('.tab');
+
+buttonContainer.addEventListener('click', function (event) {
+    if (event.target.tagName === 'BUTTON') {
+        tabs.forEach(function (tab) {
+            tab.style.display = 'none';
+        });
+
+        var buttonIndex = Array.from(buttonContainer.children).indexOf(event.target);
+
+        tabs[buttonIndex].style.display = 'block';
+    }
+});
